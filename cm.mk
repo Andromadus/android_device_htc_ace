e928c09f294d7d61608486bd497f12dea05d2cd7
synergydev <synergycomputing@gmail.com>
2012-08-02 19:56:03 -0700

    cm.mk: avoid weird inheritance

--- cm.mk
+++ cm.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, device/htc/ace/device_ace.mk)
+$(call inherit-product, device/htc/ace/BoardConfig.mk)
 
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
