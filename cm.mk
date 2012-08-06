5dcd9e24d14903522fd39d43ab9366f55c95fb72
synergy dev <synergy@codefi.re>
2012-08-06 12:41:38 -0700

    cm.mk: proper layout

--- cm.mk
+++ cm.mk
@@ -1,19 +1,20 @@
-# Inherit AOSP device configuration for ace
-$(call inherit-product, device/htc/ace/device_ace.mk)
-$(call inherit-product, device/htc/ace/BoardConfig.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+# Release Name
+PRODUCT_RELEASE_NAME := DHD
+
+TARGET_BOOTANIMATION_NAME := vertical-480x800
 
 # Inherit CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit AOSP device configuration for ace
+$(call inherit-product, device/htc/ace/device_ace.mk)
+$(call inherit-product, device/htc/ace/BoardConfig.mk)
 
 PRODUCT_NAME := cm_ace
 PRODUCT_DEVICE := ace
 PRODUCT_BRAND := HTC
 PRODUCT_MODEL := Desire HD
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_ID=JRO03H BUILD_FINGERPRINT=google/yakju/maguro:4.1.1/JRO03H/330937:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03H 330937 release-keys"
-
-PRODUCT_RELEASE_NAME := DHD
