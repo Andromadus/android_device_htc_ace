600b7fb50d3c1ad8c1dbb29ecc5cfd5fd3533710
Synergy Dev <synergycomputing@gmail.com>
2012-08-20 23:19:43 +0000

    device_ace.mk: fix media_profiles copyfile typo

--- device_ace.mk
+++ device_ace.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
     device/htc/ace/ramdisk/ueventd.spade.rc:root/ueventd.spade.rc
 	
 # Media Profiles
-PRODUCT_COPY_FILES += device/htc/msm7x30-common/media_profiles.xml:system/etc/media_profiles.xml
+PRODUCT_COPY_FILES += device/htc/ace/media_profiles.xml:system/etc/media_profiles.xml
 
 # GPS config
 PRODUCT_COPY_FILES += device/htc/ace/configs/gps.conf:system/etc/gps.conf
