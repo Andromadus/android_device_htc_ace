8ca25258d52b56291f43919667223c44645d1ae9
Synergy Dev <synergycomputing@gmail.com>
2012-08-25 22:06:54 +0000

    add old AOKP bootanimation

--- aokp.mk
+++ aokp.mk
@@ -16,5 +16,8 @@ PRODUCT_BRAND := HTC
 PRODUCT_MODEL := Desire HD
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ace BUILD_FINGERPRINT=google/yakju/maguro:4.1.1/JRO03L/330937:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03L 330937 release-keys"
 
+PRODUCT_COPY_FILES +=  \
+    device/htc/ace/prebuilt/bootanimation.zip:system/media/bootanimation.zip
+
 PRODUCT_PACKAGES += \
     Apollo
