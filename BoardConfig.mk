f4e07caf97cf486a5ebd81145870eb6a084153e3
Synergy Dev <synergycomputing@gmail.com>
2012-08-27 03:50:52 +0000

    Revert "BoardConfig: build with Linaro4.7"
    This reverts commit 30cccf99583df44b840317d0fdc723c78413aba1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 
 TARGET_KERNEL_SOURCE := kernel/htc/KangBanged-7x30
 TARGET_KERNEL_CONFIG := kangbang_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-linaro-4.7
 #TARGET_PREBUILT_KERNEL := device/htc/ace/prebuilt/kernel/kernel
 
 BOARD_HAS_LARGE_FILESYSTEM := true
