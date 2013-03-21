aeb551e6836fe4d4435803841494cb54aa554ac7
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-03-21 15:24:44 +0000

    Bluetooth libbt_vncfg addition and remove goldfish blobs (@Andromadus saga repo)
    Original commit: https://github.com/Andromadus/android_device_htc_saga/commit/6c3526769cfee4409fb5edc3678f2b40b121e2e7
    
    Change-Id: I74efa9f74d929325d290b54fd613df91c40e9c63

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,4 +76,10 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
+# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/ace/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/ace/configs/libbt_vndcfg.txt
+
+# No SDK blobs
+BUILD_EMULATOR_SENSORS_MODULE := false
+BUILD_EMULATOR_GPS_MODULE := false
