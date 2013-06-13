43ff9b30a6ecc6fe7afb5859a523cb40d55d956c
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-06-13 08:15:44 +0000

    rename device_ace.mk to device.mk
    Change-Id: I239baccbfa8f10de33e353c4391255ff1e208a04

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ TARGET_BOOTANIMATION_NAME := 480
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit AOSP device configuration for ace
-$(call inherit-product, device/htc/ace/device_ace.mk)
+$(call inherit-product, device/htc/ace/device.mk)
 
 PRODUCT_NAME := cm_ace
 PRODUCT_DEVICE := ace
